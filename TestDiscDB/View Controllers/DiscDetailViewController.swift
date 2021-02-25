57b55aeb0e3a23ed566c5a88ed911db49d3a0fdb
Benjamin Tincher <b.h.tincher@gmail.com>
2021-02-24 22:36:55 -0500

    adds link and more detail to detailVC

--- TestDiscDB/View Controllers/DiscDetailViewController.swift
+++ TestDiscDB/View Controllers/DiscDetailViewController.swift
@@ -4,40 +4,137 @@
 //
 //  Created by Benjamin Tincher on 2/24/21.
 //
-
+import SafariServices
 import UIKit
 
 class DiscDetailViewController: UIViewController {
     
     //  MARK: - Properties
-    //static let shared = DiscDetailViewController()
-    let label = UILabel()
+    @IBOutlet weak var linkButton: UIButton!
     
     var selectedDisc: Disc? {
         didSet {
-            updateView()
+            configureView()
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureView()
+        guard let disc = selectedDisc else { return }
+        
+        if disc.linkURLString == "" || disc.linkURLString.isEmpty {
+            linkButton.isHidden = true
+            return
+        }
     }
     
-   
+    //  MARK: - Actions
+    
+    @IBAction func linkButtonTapped(_ sender: Any) {
+        guard let disc = selectedDisc,
+              let linkURL = URL(string: disc.linkURLString) else { return }
+        
+        let vc = SFSafariViewController(url: linkURL)
+        
+        present(vc, animated: true, completion: nil)
+    }
     
     //  MARK: - Configure Views
     func configureView() {
-        self.view.addSubview(label)
-        label.frame = CGRect(x: 8, y: 48, width: view.frame.width - 16, height: 24)
-        label.textAlignment = .center
         
+        guard let disc = selectedDisc else { return }
+        
+        let mainStack = UIStackView()
+        mainStack.axis = .vertical
+        mainStack.alignment = .center
+        mainStack.distribution = .equalSpacing
+        mainStack.frame = CGRect(x: 8, y: 48, width: view.frame.width-16, height: 200)
+        view.addSubview(mainStack)
+        
+        let labelStack = UIStackView()
+        labelStack.axis = .vertical
+        labelStack.alignment = .center
+        labelStack.distribution = .fillEqually
+        labelStack.spacing = 16
+        labelStack.frame = CGRect(x: 0, y: 0, width: mainStack.frame.width, height: mainStack.frame.height/3*2)
+        mainStack.addArrangedSubview(labelStack)
+        
+        for index in 1...4 {
+            let label = UILabel()
+            labelStack.addArrangedSubview(label)
+            label.textAlignment = .center
+            
+            switch index {
+            case 1:
+                label.text = "Model: \(disc.model)"
+            case 2:
+                label.text = "Brand: \(disc.make)"
+            case 3:
+                label.text = "Type: \(disc.type)"
+            case 4:
+                label.text = "In Poduction: \(disc.inProduction)"
+            default:
+                return
+            }
+        }
+        
+        let flightNumsStack = UIStackView()
+        flightNumsStack.axis = .horizontal
+        flightNumsStack.alignment = .fill
+        flightNumsStack.distribution = .fillEqually
+        labelStack.frame = CGRect(x: 0, y: 0, width: mainStack.frame.width, height: mainStack.frame.height/3)
+        flightNumsStack.spacing = 24
+        mainStack.addArrangedSubview(flightNumsStack)
+        
+        for index in 1...4 {
+            
+            let ratingStack = UIStackView()
+            ratingStack.axis = .vertical
+            ratingStack.spacing = 8
+            flightNumsStack.addArrangedSubview(ratingStack)
+            
+            let titleLabel = UILabel()
+            titleLabel.textAlignment = .center
+            ratingStack.addArrangedSubview(titleLabel)
+            
+            let ratingLabel = UILabel()
+            ratingLabel.textAlignment = .center
+            ratingStack.addArrangedSubview(ratingLabel)
+            
+            switch index {
+            case 1:
+                titleLabel.text = "Speed"
+                if let speed = disc.speed {
+                    ratingLabel.text = "\(speed)"
+                } else {
+                    ratingLabel.text = "N/A"
+                }
+            case 2:
+                titleLabel.text = "Glide"
+                if let glide = disc.glide {
+                    ratingLabel.text = "\(glide)"
+                } else {
+                    ratingLabel.text = "N/A"
+                }
+            case 3:
+                titleLabel.text = "Turn"
+                if let turn = disc.turn {
+                    ratingLabel.text = "\(turn)"
+                } else {
+                    ratingLabel.text = "N/A"
+                }
+            case 4:
+                titleLabel.text = "Fade"
+                if let fade = disc.fade {
+                    ratingLabel.text = "\(fade)"
+                } else {
+                    ratingLabel.text = "N/A"
+                }
+            default:
+                return
+            }
+        }
     }
     
-    func updateView() {
-        guard let disc = selectedDisc else { return }
-        self.label.text = disc.model
-    }
-
 }
